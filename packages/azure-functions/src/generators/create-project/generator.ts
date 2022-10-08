a3a6cfa54e8973c2ae5871306a870e2ea726487a
Fabian Beyerlein <dev@beyerleinf.de>
2022-10-09 00:37:42 +0200

    feat(generators): Implement create-project
    Closes #4

--- packages/azure-functions/src/generators/create-project/generator.ts
+++ packages/azure-functions/src/generators/create-project/generator.ts
@@ -1,12 +1,16 @@
 import {
+  addDependenciesToPackageJson,
   addProjectConfiguration,
   formatFiles,
   generateFiles,
   getWorkspaceLayout,
+  installPackagesTask,
   names,
   offsetFromRoot,
   Tree,
 } from '@nrwl/devkit';
+import { jestProjectGenerator } from '@nrwl/jest';
+import { Linter, lintProjectGenerator } from '@nrwl/linter';
 import * as path from 'path';
 import { AzureFunctionsGeneratorSchema } from './schema';
 
@@ -26,7 +30,7 @@ function normalizeOptions(
     ? `${names(options.directory).fileName}/${name}`
     : name;
   const projectName = projectDirectory.replace(new RegExp('/', 'g'), '-');
-  const projectRoot = `${getWorkspaceLayout(tree).libsDir}/${projectDirectory}`;
+  const projectRoot = `${getWorkspaceLayout(tree).appsDir}/${projectDirectory}`;
   const parsedTags = options.tags
     ? options.tags.split(',').map((s) => s.trim())
     : [];
@@ -45,8 +49,9 @@ function addFiles(tree: Tree, options: NormalizedSchema) {
     ...options,
     ...names(options.name),
     offsetFromRoot: offsetFromRoot(options.projectRoot),
-    template: '',
+    tmpl: '',
   };
+
   generateFiles(
     tree,
     path.join(__dirname, 'files'),
@@ -55,22 +60,67 @@ function addFiles(tree: Tree, options: NormalizedSchema) {
   );
 }
 
+function addDependencies(tree: Tree) {
+  addDependenciesToPackageJson(
+    tree,
+    {},
+    { '@azure/functions': '3.0.0', typescript: '4' }
+  );
+}
+
+function addProjectConfig(tree: Tree, normalizedOptions: NormalizedSchema) {
+  addProjectConfiguration(tree, normalizedOptions.projectName, {
+    root: normalizedOptions.projectRoot,
+    projectType: 'application',
+    sourceRoot: `${normalizedOptions.projectRoot}`,
+    targets: {
+      build: {
+        executor: '@beyerleinf/nx-azure-functions:build',
+      },
+      serve: {
+        executor: '@beyerleinf/nx-azure-functions:serve',
+      },
+    },
+    tags: normalizedOptions.parsedTags,
+  });
+}
+
+async function addLinting(tree: Tree, normalizedOptions: NormalizedSchema) {
+  return lintProjectGenerator(tree, {
+    project: normalizedOptions.projectName,
+    skipFormat: false,
+    linter: Linter.EsLint,
+  });
+}
+
+async function addJest(tree: Tree, normalizedOptions: NormalizedSchema) {
+  return jestProjectGenerator(tree, {
+    project: normalizedOptions.projectName,
+    testEnvironment: 'node',
+    setupFile: 'none',
+    compiler: 'tsc',
+    supportTsx: false,
+    js: false,
+    skipSerializers: true,
+    skipFormat: true,
+    skipPackageJson: false,
+  });
+}
+
 export default async function (
   tree: Tree,
   options: AzureFunctionsGeneratorSchema
 ) {
   const normalizedOptions = normalizeOptions(tree, options);
-  addProjectConfiguration(tree, normalizedOptions.projectName, {
-    root: normalizedOptions.projectRoot,
-    projectType: 'library',
-    sourceRoot: `${normalizedOptions.projectRoot}/src`,
-    targets: {
-      build: {
-        executor: '@beyerleinf/nx-azure-functions:build',
-      },
-    },
-    tags: normalizedOptions.parsedTags,
-  });
+
+  addDependencies(tree);
+  addProjectConfig(tree, normalizedOptions);
   addFiles(tree, normalizedOptions);
+
+  await addLinting(tree, normalizedOptions);
+  await addJest(tree, normalizedOptions);
+
   await formatFiles(tree);
+
+  installPackagesTask(tree);
 }
